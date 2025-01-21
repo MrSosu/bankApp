117921fd1253caf6e4ce5e120099ee427fa27dec
m_adriani <m.adriani@jdk.it>
2025-01-21 09:42:17 +0100

    pushetto pushettino pusher

--- src/main/java/com/example/bankApp/domain/dto/requests/CreateUtenteRequest.java
+++ src/main/java/com/example/bankApp/domain/dto/requests/CreateUtenteRequest.java
@@ -1,17 +1,34 @@
 package com.example.bankApp.domain.dto.requests;
 
 import com.example.bankApp.domain.entities.Comune;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
 public record CreateUtenteRequest(
+        @NotBlank(message = "Il nome non può essere blank o null")
         String nome,
+        @NotBlank(message = "Il cognome non può essere blank o null")
         String cognome,
+        @Email(message = "Email non valida")
         String email,
+        @Pattern(
+                regexp = "^\\+?[0-9]{1,3}?[-.\\s]?[0-9]{1,4}[-.\\s]?[0-9]{1,4}[-.\\s]?[0-9]{1,9}$\n",
+                message = "Telefono non valido")
         String telefono,
+        @Pattern(
+                regexp = "^[A-Z]{6}[0-9]{2}[A-EHLMPR-T][0-9]{2}[A-Z][0-9]{3}[A-Z]$\n",
+                message = "Codice fiscale non valido")
         String codiceFiscale,
+        @NotBlank(message = "L'indirizzo non può essere null o blank")
         String indirizzo,
+        @Past(message = "La data di nascita deve essere nel passato")
         LocalDate dataNascita,
+        @NotNull(message = "il comune deve essere presente")
         Comune comune
 ) {
 }
--- src/main/java/com/example/bankApp/domain/dto/requests/UpdateUtenteRequest.java
+++ src/main/java/com/example/bankApp/domain/dto/requests/UpdateUtenteRequest.java
@@ -1,14 +1,23 @@
 package com.example.bankApp.domain.dto.requests;
 
 import com.example.bankApp.domain.entities.Comune;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 
 import java.time.LocalDate;
 
 public record UpdateUtenteRequest(
         String nome,
         String cognome,
+        @Nullable
+        @Pattern(
+                regexp = "^\\+?[0-9]{1,3}?[-.\\s]?[0-9]{1,4}[-.\\s]?[0-9]{1,4}[-.\\s]?[0-9]{1,9}$\n",
+                message = "Telefono non valido")
         String telefono,
         String indirizzo,
+        @Nullable
+        @Past(message = "La data di nascita non può essere nel passato")
         LocalDate dataNascita,
         Comune comune
 ) {
