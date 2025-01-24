589c89db0be1937eae6243dadad8f35b8636e803
m_adriani <m.adriani@jdk.it>
2025-01-24 12:26:47 +0100

    login - seconda parte

---
+++ src/main/java/com/example/bankApp/configuration/ApplicationConfig.java
@@ -0,0 +1,47 @@
+package com.example.bankApp.configuration;
+
+import com.example.bankApp.repositories.UtenteRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Autowired
+    private UtenteRepository utenteRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> utenteRepository.findByEmail(username)
+                .orElseThrow(() -> new EntityNotFoundException("Utente con email " + username + " non trovato"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() { // -> oggetto necessario a estrapolare i dati dell'utente
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+}
---
+++ src/main/java/com/example/bankApp/security/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.bankApp.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // definisco i miei endpoints pubblici
+        List<String> publicEndpoints = List.of(
+            "/app/v1/auth/**"
+        );
+        // passo la lista al jwtAuthFilter
+        jwtAuthFilter.setPublicEndpoints(publicEndpoints);
+        // creo la catena di filtri
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req
+                        .requestMatchers(publicEndpoints.toArray(new String[0])) // per gli endpoint pubblici
+                        .permitAll() // permetti sempre l'accesso
+                        .anyRequest() // per ogni altra richiesta
+                        .authenticated() // richiedi l'autenticazione
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+
+}
