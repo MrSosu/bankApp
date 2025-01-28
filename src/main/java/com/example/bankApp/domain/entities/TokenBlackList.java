79af2060274e768d507193c64fb97286e95bc500
m_adriani <m.adriani@jdk.it>
2025-01-28 10:00:48 +0100

    logout

--- src/main/java/com/example/bankApp/domain/entities/TokenBlackList.java
+++ src/main/java/com/example/bankApp/domain/entities/TokenBlackList.java
@@ -14,6 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -24,7 +25,7 @@ import java.time.LocalDateTime;
 @Builder
 @Entity
 @Table(name = "token_bl")
-@EntityListeners(TokenBlackList.class)
+@EntityListeners(AuditingEntityListener.class)
 public class TokenBlackList {
 
     @Id
